b87832f9b8f53fe954aea97c20d615dda900502c
lamiru <lamiru@gmail.com>
2018-09-19 17:59:45 +0900

    edit main admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+class ListAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'title',
+        'created_at', 'updated_at',
+    )
+
+admin.site.register(List, ListAdmin)
+
+
+class CardAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'title', 'contents', 'list', 'order',
+        'created_at', 'updated_at',
+    )
+
+admin.site.register(Card, CardAdmin)
