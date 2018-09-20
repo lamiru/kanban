dbaec3e5067dc17a94dfb53a24da7aa32af4d9c1
lamiru <lamiru@gmail.com>
2018-09-20 18:29:11 +0900

    get rendering data from database

--- README.md
+++ README.md
@@ -23,18 +23,11 @@ Kanban
 - 카드를 드래그 & 드랍 방식으로 리스트 사이에서 옮기기. 순서 바꾸기
 - 카드가 속한 리스트와 순서를 하나의 JSON 배열로 만들고 그 데이터를 DB에 ajax로 입출력
 - List에 대한 드래그 & 드랍 방식의 순서 바꾸기
-- printLists: 리스트 데이터를 화면에 렌더링 (DB 연동 미구현)
-- printCards: 카드 데이터를 카드가 속한 각 리스트에 렌더링 (DB 연동 미구현)
-- printUsers: 유저 데이터를 유저가 속한 각 카드에 렌더링 (DB 연동 미구현)
+- printLists: 리스트 데이터를 화면에 렌더링
+- printCards: 카드 데이터를 카드가 속한 각 리스트에 렌더링
+- printUsers: 유저 데이터를 유저가 속한 각 카드에 렌더링
 
 #### 구현해야 할 기능 1
-- printLists: ajax를 통하여 List 데이터를 DB에서 출력 후 렌더링
-- printCards: ajax를 통하여 Card 데이터를 DB에서 출력 후 렌더링
-- printUsers: ajax를 통하여 User 데이터를 DB에서 출력 후 렌더링
-- (위 세가지 기능은 Django Models에 입력되어 있는 데이터를 호출하여 api에서 JSONResponse해주는 view를 만들고 클라이언트에서 ajax로 호출한 후에 렌더링)
-- (0.5일 예상)
-
-#### 구현해야 할 기능 2
 - List에 대한 Create, Edit, Delete
 - Card에 대한 Create, Edit, Delete
 - User에 대한 Create, Edit, Delete (회원가입, 프로필 수정, 회원탈퇴)
@@ -44,6 +37,6 @@ Kanban
 - (User에 대한 Create, Edit, Delete는 일반적인 form 입력을 통한 POST 전송방식)
 - (2일 예상)
 
-#### 구현해야 할 기능 3
+#### 구현해야 할 기능 2
 - Card에 대한 Edit에서 User 추가/삭제 ajax로 구현
 - (0.5일 예상)
--- api/views.py
+++ api/views.py
@@ -3,19 +3,55 @@ from django.core.serializers import serialize
 from django.http import Http404, JsonResponse, QueryDict
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.models import User
 from main.models import *
 
 
 def items(request):
-    list_json = json.loads(serialize('json', List.objects.all()))
-    card_json = json.loads(serialize('json', Card.objects.all()))
+    list_dict = json.loads(serialize('json', List.objects.all()))
+    list_json = {}
+    for list in list_dict:
+        list_json[list['pk']] = {
+            'pk': list['pk'],
+            'title': list['fields']['title'],
+            'created_at': list['fields']['created_at'],
+            'updated_at': list['fields']['updated_at'],
+        }
 
-    return JsonResponse({'lists': list_json, 'cards': card_json})
+    card_dict = json.loads(serialize('json', Card.objects.all()))
+    card_json = {}
+    for card in card_dict:
+        card_json[card['pk']] = {
+            'pk': card['pk'],
+            'title': card['fields']['title'],
+            'created_at': card['fields']['created_at'],
+            'updated_at': card['fields']['updated_at'],
+        }
+
+    user_dict = json.loads(serialize('json', User.objects.all()))
+    user_json = {}
+    for user in user_dict:
+        user_json[user['pk']] = {
+            'pk': user['pk'],
+            'username': user['fields']['username'],
+            'first_name': user['fields']['first_name'],
+            'last_name': user['fields']['last_name'],
+        }
+
+    return JsonResponse({'lists': list_json, 'cards': card_json, 'users': user_json})
 
 def lists(request):
-    list_json = json.loads(serialize('json', List.objects.all()))
+    list_dict = json.loads(serialize('json', List.objects.all()))
+    list_json = {}
+    for list in list_dict:
+        list_json[list['pk']] = {
+            'pk': list['pk'],
+            'title': list['fields']['title'],
+            'created_at': list['fields']['created_at'],
+            'updated_at': list['fields']['updated_at'],
+        }
 
-    return JsonResponse({'lists': list_json})
+    return JsonResponse(list_json)
 
 def list_card_order(request, pk):
     list_card_order = get_object_or_404(ListCardOrder, pk=1)
